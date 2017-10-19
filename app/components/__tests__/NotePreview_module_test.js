a5ee17acb6909c8e553651c41a0cc4ebcadb3687
Yu Hoshino <yhoshino11@gmail.com>
2017-10-20 06:09:13 +0900

    refactored test code

--- app/components/__tests__/NotePreview_module_test.js
+++ app/components/__tests__/NotePreview_module_test.js
@@ -4,24 +4,17 @@ test('split single markdown text to markdown and code array', () => {
     const md = "# foo \n*bar* ~baz~\n"
     const code = "```\n$ echo 'foo bar baz'\n```"
 
-    const input0 = ''
-    expect(textToMarkdownAndCodeArray(input0).length).toBe(1)
+    const fixtures = [
+      { param: '', outputArrayLength: 1},
+      { param: md, outputArrayLength: 1},
+      { param: code, outputArrayLength: 1},
+      { param: md+md, outputArrayLength: 1},
+      { param: md+code, outputArrayLength: 2},
+      { param: code+code, outputArrayLength: 2},
+      { param: md+code+md+md+code+md+md+code+code+md, outputArrayLength: 8}
+    ]
 
-    const input1 = md
-    expect(textToMarkdownAndCodeArray(input1).length).toBe(1)
-
-    const input2 = code
-    expect(textToMarkdownAndCodeArray(input2).length).toBe(1)
-
-    const input3 = md+md
-    expect(textToMarkdownAndCodeArray(input3).length).toBe(1)
-
-    const input4 = md+code
-    expect(textToMarkdownAndCodeArray(input4).length).toBe(2)
-
-    const input5 = code+code
-    expect(textToMarkdownAndCodeArray(input5).length).toBe(2)
-
-    const input6 = code+code+md+md+code+md+md+code+code+md
-    expect(textToMarkdownAndCodeArray(input6).length).toBe(8)
+    fixtures.forEach((testCase) => {
+      expect(textToMarkdownAndCodeArray(testCase.param).length).toBe(testCase.outputArrayLength)
+    })
 })
